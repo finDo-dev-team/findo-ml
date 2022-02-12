41d2e2a1944f45705e7eeddb7429af93a1e5c5d4
CLebien <nd.clebien>
2022-02-12 14:05:51 +0100

    Importation puis nettoyage des données

--- kmeans_model.py
+++ kmeans_model.py
@@ -5,3 +5,24 @@ from sklearn import preprocessing
 from sklearn import metrics
 import pickle
 import json
+
+# Importer les donnees fakeUsers depuis l'api laravel
+'''
+url =
+'''
+data1 = pd.read_table("FakeUsers.csv", sep=",")
+data = data1.drop(['age', 'ville'], axis=1)
+
+'''
+print(data)
+print(data.dtypes)
+'''
+
+#Labellisation des champ textes avec Encoder1
+Encoder =preprocessing.LabelEncoder()
+data["prenom"]=Encoder.fit_transform(data["prenom"])
+data["nom"]=Encoder.fit_transform(data["nom"])
+
+'''
+data["ville"]=Encoder.fit_transform(data["ville"])
+'''
