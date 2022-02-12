db4ef39c597ff4ce969ef57b872b741a419c918b
CLebien <nd.clebien>
2022-02-12 14:04:01 +0100

    Importation des bibliothèques

---
+++ kmeans_model.py
@@ -0,0 +1,7 @@
+# Importation des biblithèques nécessaires
+import pandas as pd
+from sklearn.cluster import KMeans
+from sklearn import preprocessing
+from sklearn import metrics
+import pickle
+import json
